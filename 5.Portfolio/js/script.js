68276f21ff28d88ffa69cbb5289fd6527d719b63
Krlosh85 <carlitos_herrera_jr@hotmail.com>
2021-06-29 22:23:40 -0600

    Portfolio Completed

--- 5.Portfolio/js/script.js
+++ 5.Portfolio/js/script.js
@@ -23,3 +23,36 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*Scroll Reveal Animation using Scroll Reveal Library*/ 
+const scrollr = ScrollReveal({
+    origion: 'top',
+    distance: '70px',
+    duration: 2000,
+    reset:true
+})
+
+
+/*Scroll Home*/
+
+scrollr.reveal('.home-title',{})
+scrollr.reveal('.button', {delay:200})
+scrollr.reveal('.home-img', {delay:200})
+scrollr.reveal('.home-social-icon', {interval:200})
+
+/*Scroll About*/
+scrollr.reveal('.about-img',{})
+scrollr.reveal('.about-subtitle', {delay:200})
+scrollr.reveal('.about-text', {delay:300})
+
+/*Scroll Skills*/
+scrollr.reveal('.skills-subtitle',{})
+scrollr.reveal('.skills-text', {delay:200})
+scrollr.reveal('.skills-data', {interval:200})
+
+/*Scroll Projects*/
+scrollr.reveal('.projects-img', {interval:200})
+
+/*Scroll Contact*/
+scrollr.reveal('.contact-input', {interval:200})
+
